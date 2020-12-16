e833591eb94c97b8c57398260a99f60a5b407d6a
Jonathan Garcia <jagspurs7@gmail.com>
2020-12-15 18:05:18 -0600

    added basic components to test the componenets functionallity of react-native. need to refactor my componets to seperate conceren and display the info needed.

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, Button } from 'react-native';
+import apiCalls from './apiCalls.js'
+const testName = 'laurenzside';
 
 export default function App() {
+  const [channelId , setChannelId] = useState('');
+  const [thumbnail , setThumbnail] = useState(null);
+  const [description , setDescription] = useState('');
+  const [title , setTitle] = useState('');
+  const [youtubersName, onChangeText] = useState('');
+  const [wantsData, setWantsData] = useState(false);
+
+
+
+  useEffect(() =>{
+    apiCalls.getChannel(testName)
+    .then (({ data }) => {
+      setChannelId(data.items[0].id)
+      setThumbnail(data.items[0].snippet.thumbnails.default.url)
+      setDescription(data.items[0].snippet.description)
+      setTitle(data.items[0].snippet.title)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to viewtubeWatchDog</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text} >welcome to viewtubeWatchDog!!</Text>
+      <TextInput
+      style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={youtubersName}
+      />
+      <Image source = {{
+        width: 60,
+        height: 60,
+        uri: thumbnail ? thumbnail: null,
+      }}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  text: {
+    height: 100,
+    color: 'blue'
+  }
 });
--- apiCalls.js
+++ apiCalls.js
@@ -1,2 +1,16 @@
 import axios from 'axios';
+const keys = require('./config.js');
 
+const searchChannelUrl = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics%2C%20localizations%2C%20topicDetails%2C%20brandingSettings&forUsername=`
+const restOfParams = `&maxResults=20&prettyPrint=true&key=${keys.ytKey}`
+
+
+const getChannel = (youtuberName) => {
+  return axios.get(`${searchChannelUrl}${youtuberName}${restOfParams}`);
+}
+
+const apiCalls = {
+  getChannel : getChannel,
+}
+
+export default apiCalls
